1484db40e4418e1f15dd0d554ff3d4ecee37af9b
Stephen Chung <spchung95@gmail.com>
2025-07-03 22:06:16 -0400

    clean

--- integrations/interface.go
+++ integrations/interface.go
@@ -7,12 +7,14 @@ import (
 )
 
 type Integration interface {
-	// GetEventKey is used to get the event key from the request.
-	// Event key is typically used to identify the user that triggered
-	// a webhook, which then gets used to lookup where to forward the request.
+	// GetEventKey is used to derive a key from the request.
+	// This key is typically used to identify the user that triggered a
+	// webhook, which helps whoxy lookup where to forward the request.
 	GetEventKey(*http.Request) (string, error)
 }
 
 var (
 	_ Integration = (*clerk.ClerkIntegration)(nil)
+	// Add more integrations here
+	// Plugin system?
 )
