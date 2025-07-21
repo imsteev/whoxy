e76893d17e075a59d1a854448db44ebf450398e6
Stephen Chung <spchung95@gmail.com>
2025-07-20 23:43:15 -0400

    clean

--- whoxy-app/src/utils.ts
+++
@@ -1,7 +0,0 @@
-export const forwardRequestTo = async (url: string, request: Request) => {
-  return fetch(url, {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-  });
-};
